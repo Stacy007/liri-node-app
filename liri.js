3ddf6948e6afb9245aa863d10c4f94bf3e5bcb37
Stacy007 <stacy.richardson17@gmail.com>
2018-07-23 13:37:19 -0700

    here is liri hw

--- liri.js
+++ liri.js
@@ -5,13 +5,14 @@ var fs = require("fs");
 var Twitter = require("twitter");
 // Spotify package
 var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
 //this one is for OMDb
 var request = require("request");
-var movie = process.argv[2] ;
+var input = process.argv[2];
+var input2 = process.argv[3];
 
-var movieName = function(gitMovie) {
-request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+if (input === "movie-this") {
+// var movieName = function(gitMovie) {
+request("http://www.omdbapi.com/?t=" + input2 + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
 // * `movie-this`'<movie name here>
   if (!error && response.statusCode === 200) {
     var movieThis = JSON.parse(body);
@@ -24,24 +25,41 @@ request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", 
     console.log("Plot: " + movieThis.Plot);
     console.log("Actors: " + movieThis.Actors);
   }
-})};
+})}
 
-// * `my-tweets` - show last 20 tweets
-var client = new Twitter(keys.twitter);
-var myTweets = function(getTweets) {
-
+else if (input === "my-tweets") {
+    var client = new Twitter(keys.twitter);
+    var params = {screen_name: 'x21082133'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    for (var i = 0; i < tweets.length; i++){
+    console.log(tweets[i].text);
+    console.log(tweets[i].created_at);
+  }}
+})
 }
-
-// * `spotify-this-song` '<song name here>'
-var spotify = new Spotify(keys.spotify);
-var spotifyThisSong = function(getSpotify) {
-    
+else if (input === "spotify-this-song") {
+  var spotify = new Spotify(keys.spotify);
+  spotify.search({ type: 'track', query: input2 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+var songs = data.tracks.items
+for (var i = 0; i < songs.length; i++){
+  console.log(songs[i].name);
+  console.log(songs[i].album.name);
+  console.log(songs[i].artists[0].name);
+  console.log(songs[i].preview_url);
+  }})
 }
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-// If no song is provided then your program will default to "The Sign" by Ace of Base.
+else if (input === "do-what-it-says") {
+  fs.readFile("random.text", "utf8", function(error, data) {
+    console.log(data);
+  })
+}
+else {
+  console.log("your doing it wrong");
+}
 
 
 
